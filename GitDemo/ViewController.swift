2816a820b47dcd168dfeec0e6853f397a6af23a2
Artem <nwlf.420@gmail.com>
2021-11-30 13:15:13 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by pro2017 on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
